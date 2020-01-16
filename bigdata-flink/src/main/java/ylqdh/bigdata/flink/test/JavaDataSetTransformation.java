6c80ba33119133ef7dd4faf949709eaecefd91e2
1shi123de1 <ylqdh2014@qq.com>
2020-01-16 11:41:33 +0800

    flink的join/outerjoin/cross转换例子

--- bigdata-flink/src/main/java/ylqdh/bigdata/flink/test/JavaDataSetTransformation.java
+++ bigdata-flink/src/main/java/ylqdh/bigdata/flink/test/JavaDataSetTransformation.java
@@ -1,18 +1,16 @@
 package ylqdh.bigdata.flink.test;
 
-import org.apache.avro.generic.GenericData;
-import org.apache.flink.api.common.functions.FilterFunction;
-import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.common.functions.MapPartitionFunction;
+import org.apache.flink.api.common.functions.*;
 import org.apache.flink.api.common.operators.Order;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.operators.DataSource;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.util.Collector;
-import scala.Int;
 
+import javax.lang.model.type.ExecutableType;
+import javax.xml.crypto.Data;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,7 +30,10 @@ public class JavaDataSetTransformation {
 //        mapPartitionFunction(env);
 //        firstFunction(env);
 //        flatMapFunction(env);
-        distinctFunction(env);
+//        distinctFunction(env);
+//        joinFunction(env);
+//        outerJoinFunction(env);
+        crossFunction(env);
     }
 
     // map
@@ -75,6 +76,7 @@ public class JavaDataSetTransformation {
         System.out.println("filter function : 过滤出大于5的值");
     }
 
+    // mapPartition
     public static void mapPartitionFunction(ExecutionEnvironment env) throws Exception {
         List<String> students = new ArrayList<>();
         for (int x=0; x<100; x++) {
@@ -177,6 +179,115 @@ public class JavaDataSetTransformation {
                 }
             }
         }).distinct().print();
+    }
+
+    // join
+    public static void joinFunction(ExecutionEnvironment env) throws Exception {
+        List<Tuple2<Integer,String>> info1 = new ArrayList<>();
+        info1.add(new Tuple2<>(1,"可达鸭"));
+        info1.add(new Tuple2<>(2,"ylqdh"));
+        info1.add(new Tuple2<>(3,"皮卡丘"));
+        info1.add(new Tuple2<>(4,"鲤鱼王"));
+
+        List<Tuple2<Integer,String>> info2 = new ArrayList<>();
+        info2.add(new Tuple2<>(1,"深圳"));
+        info2.add(new Tuple2<>(2,"广州"));
+        info2.add(new Tuple2<>(3,"上海"));
+        info2.add(new Tuple2<>(5,"杭州"));
+
+        DataSource<Tuple2<Integer,String>> data1 = env.fromCollection(info1);
+        DataSource<Tuple2<Integer,String>> data2 = env.fromCollection(info2);
+
+        data1.join(data2).where(0).equalTo(0)
+                .with(new JoinFunction<Tuple2<Integer,String>, Tuple2<Integer,String>, Tuple3<Integer,String,String>>() {
+                    @Override
+                    public Tuple3<Integer, String, String> join(Tuple2<Integer, String> first, Tuple2<Integer, String> second) throws Exception {
+                        return new Tuple3<Integer,String,String>(first.f0,first.f1,second.f1);
+                    }
+                })
+                .print();
+    }
+
+    // outer join
+    public static void outerJoinFunction (ExecutionEnvironment env) throws Exception {
+        List<Tuple2<Integer,String>> info1 = new ArrayList<>();
+        info1.add(new Tuple2<>(1,"可达鸭"));
+        info1.add(new Tuple2<>(2,"ylqdh"));
+        info1.add(new Tuple2<>(3,"皮卡丘"));
+        info1.add(new Tuple2<>(4,"鲤鱼王"));
+
+        List<Tuple2<Integer,String>> info2 = new ArrayList<>();
+        info2.add(new Tuple2<>(1,"深圳"));
+        info2.add(new Tuple2<>(2,"广州"));
+        info2.add(new Tuple2<>(3,"上海"));
+        info2.add(new Tuple2<>(5,"杭州"));
+
+        DataSource<Tuple2<Integer,String>> data1 = env.fromCollection(info1);
+        DataSource<Tuple2<Integer,String>> data2 = env.fromCollection(info2);
+
+        System.out.println("~~~~~~~left outer join~~~~~~~~~~~~");
+        // left outer join
+        data1.leftOuterJoin(data2).where(0).equalTo(0)
+                .with(new JoinFunction<Tuple2<Integer,String>, Tuple2<Integer,String>, Tuple3<Integer,String,String>>() {
+                    @Override
+                    public Tuple3<Integer, String, String> join(Tuple2<Integer, String> first, Tuple2<Integer, String> second) throws Exception {
+                       if (second == null) {
+                           return new Tuple3<>(first.f0,first.f1,"--");
+                       } else {
+                           return new Tuple3<Integer,String,String>(first.f0,first.f1,second.f1);
+                       }
+                    }
+                })
+                .print();
+
+        System.out.println("~~~~~~~~right outer join~~~~~~~~~~~~");
+        // right outer join
+        data1.rightOuterJoin(data2).where(0).equalTo(0)
+                .with(new JoinFunction<Tuple2<Integer,String>, Tuple2<Integer,String>, Tuple3<Integer,String,String>>() {
+                    @Override
+                    public Tuple3<Integer, String, String> join(Tuple2<Integer, String> first, Tuple2<Integer, String> second) throws Exception {
+                        if (first == null) {
+                            return new Tuple3<>(second.f0,"--",second.f1);
+                        } else {
+                            return new Tuple3<Integer,String,String>(first.f0,first.f1,second.f1);
+                        }
+                    }
+                })
+                .print();
+
+        System.out.println("~~~~~~~~~full join~~~~~~~~~~~");
+        // left outer join
+        data1.fullOuterJoin(data2).where(0).equalTo(0)
+                .with(new JoinFunction<Tuple2<Integer,String>, Tuple2<Integer,String>, Tuple3<Integer,String,String>>() {
+                    @Override
+                    public Tuple3<Integer, String, String> join(Tuple2<Integer, String> first, Tuple2<Integer, String> second) throws Exception {
+                        if (second == null) {
+                            return new Tuple3<>(first.f0,first.f1,"--");
+                        } else if (first == null) {
+                            return new Tuple3<>(second.f0,"--",second.f1);
+                        } else {
+                            return new Tuple3<Integer,String,String>(first.f0,first.f1,second.f1);
+                        }
+                    }
+                })
+                .print();
+    }
+
+    // cross
+    public static void crossFunction (ExecutionEnvironment env) throws Exception {
+        List<String> info1 = new ArrayList<>();
+        info1.add("team A");
+        info1.add("team B");
+
+        List<Integer> info2 = new ArrayList<>();
+        info2.add(0);
+        info2.add(1);
+        info2.add(2);
+
+        DataSource<String> data1 = env.fromCollection(info1);
+        DataSource<Integer> data2 = env.fromCollection(info2);
+
+        data1.cross(data2).print();
     }
 
 }
--- bigdata-flink/src/main/scala/ylqdh/bigdata/flink/test/DataSetTransformationApp.scala
+++ bigdata-flink/src/main/scala/ylqdh/bigdata/flink/test/DataSetTransformationApp.scala
@@ -12,6 +12,7 @@ import scala.collection.mutable.ListBuffer
   * @Author ylqdh
   * @Date 2020/1/14 18:04
   */
+//noinspection ScalaDocUnknownTag
 object DataSetTransformationApp {
   def main(args: Array[String]): Unit = {
 
@@ -22,7 +23,10 @@ object DataSetTransformationApp {
 //    mapPartitionFunction(env)
 //    firstFunction(env)
 //    flatMapFunction(env)
-    distinctFunction(env)
+//    distinctFunction(env)
+//    joinFunction(env)
+//    outerJoinFunction(env)
+    crossFunction(env)
   }
 
 
@@ -140,4 +144,102 @@ object DataSetTransformationApp {
       .distinct()
       .print()
   }
+
+  // join,根据指定条件(在这里是where和equalTO指定的列位置为key),把两个集合的key相同的结果取出来
+  def joinFunction (env:ExecutionEnvironment): Unit = {
+    val info1 = ListBuffer[(Int,String)]()
+    info1.append((1,"可达鸭"))
+    info1.append((2,"ylqdh"))
+    info1.append((3,"皮卡丘"))
+    info1.append((4,"鲤鱼王"))
+
+    val info2 = ListBuffer[(Int,String)]()
+    info2.append((1,"深圳"))
+    info2.append((2,"广州"))
+    info2.append((3,"上海"))
+    info2.append((4,"杭州"))
+
+    val data1 = env.fromCollection(info1)
+    val data2 = env.fromCollection(info2)
+
+    data1.join(data2).where(0).equalTo(0)
+        .apply((x,y) => {
+          (x._1,x._2,y._2)
+        })
+        .print()
+  }
+
+  // Outjoin,跟sql语句中的left join,right join,full join意思一样
+  // leftOuterJoin,跟join一样，但是左边集合的没有关联上的结果也会取出来,没关联上的右边为null
+  // rightOuterJoin,跟join一样,但是右边集合的没有关联上的结果也会取出来,没关联上的左边为null
+  // fullOuterJoin,跟join一样,但是两个集合没有关联上的结果也会取出来,没关联上的一边为null
+  def outerJoinFunction (env:ExecutionEnvironment): Unit = {
+    val info1 = ListBuffer[(Int,String)]()
+    info1.append((1,"可达鸭"))
+    info1.append((2,"ylqdh"))
+    info1.append((3,"皮卡丘"))
+    info1.append((4,"鲤鱼王"))
+
+    val info2 = ListBuffer[(Int,String)]()
+    info2.append((1,"深圳"))
+    info2.append((2,"广州"))
+    info2.append((3,"上海"))
+    info2.append((5,"杭州"))
+
+    val data1 = env.fromCollection(info1)
+    val data2 = env.fromCollection(info2)
+
+    println("~~~~~~~left outer join~~~~~~~~~~")
+    // leftOuterJoin
+    data1.leftOuterJoin(data2).where(0).equalTo(0)
+      .apply((x,y) => {
+        if (y == null) {
+          (x._1,x._2,"--")
+        } else {
+          (x._1,x._2,y._2)
+        }
+      })
+      .print()
+
+    println("~~~~~~~~~~~right outer join~~~~~~~~~~~~~ ")
+    // rightOuterJoin
+    data1.rightOuterJoin(data2).where(0).equalTo(0)
+      .apply( (x,y) => {
+        if ( x == null ) {
+          (y._1,"--",y._2)
+        } else {
+          (x._1,x._2,y._2)
+        }
+      })
+      .print()
+
+    println("~~~~~~~~~full join~~~~~~~~~~~~")
+    // fullOuterJoin
+    data1.fullOuterJoin(data2).where(0).equalTo(0)
+      .apply( (x,y) => {
+        if ( x == null ) {
+          (y._1,"--",y._2)
+        } else if (y == null) {
+          (x._1,x._2,"--")
+        }
+        else {
+          (x._1,x._2,y._2)
+        }
+      })
+      .print()
+  }
+
+  // cross,求两个集合的笛卡尔积,得到的结果数为：集合1的条数 乘以 集合2的条数
+  // 笛卡尔积得到的结果会很大，生产中要慎用。这里模拟两个队伍打比赛,bo3,那么一个队来说，分数可能的结果
+  def crossFunction (env:ExecutionEnvironment): Unit = {
+    val info1 = List[String]("team A","team B")
+    val info2 = List[Int](0,1,2)
+
+    val data1 = env.fromCollection(info1)
+    val data2 = env.fromCollection(info2)
+
+    data1.cross(data2).print()
+  }
+
+
 }
